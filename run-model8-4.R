d17437726ed45dc0288117a4d7100a0f3ffde2ea
akiabe <62723205+akiabe@users.noreply.github.com>
2021-06-28 06:35:08 +0900

    Add files via upload

---
+++ run-model8-4.R
@@ -0,0 +1,10 @@
+library(rstan)
+
+d <- read.csv('data-salary-2.txt')
+N <- nrow(d)
+K <- 4
+data <- list(N=N, K=K, X=d$X, Y=d$Y, KID=d$KID)
+fit4 <- stan(file='model8-4.stan', data=data, seed=1234)
+fit4
+
+save.image('result-model8-4.RData')
