2645e3934fa432c22621b9814c493416f91f26ea
akiabe <62723205+akiabe@users.noreply.github.com>
2021-07-08 06:55:16 +0900

    Add files via upload

---
+++ fig7-8.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+
+d <- read.csv(file='data-50m.txt')
+d$Age <- as.factor(d$Age)
+head(d, n=5)
+
+p <- ggplot(data=d, aes(x=Weight, y=Y)) + 
+  theme_bw(base_size=18) + 
+  geom_point(shape=1, size=2)
+
+p
+
+ggsave(file='fig7-8-left.png', plot=p, dpi=300, w=3.8, h=3)
+
+p <- ggplot(data=d, aes(x=Weight, y=Y, shape=Age)) + 
+  theme_bw(base_size=18) + 
+  geom_point(size=2) + 
+  scale_shape_manual(values=c(3, 1, 2, 4, 5, 6))
+
+p
+
+ggsave(file='fig7-8-right.png', plot=p, dpi=300, w=4.5, h=3)
